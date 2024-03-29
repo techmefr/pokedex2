223091bb899edec582e12d3c32ef0e2a01043df2
techmefr <compigni.gaetan@gmail.com>
2024-03-29 12:08:28 +0100

    finish first component

---
+++ pokedex2/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PokemonCard from "./component/PokemonCard";
+
+function App() {
+  return (
+    <div>
+      <PokemonCard />
+    </div>
+  );
+}
+
+export default App;
